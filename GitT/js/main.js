c231e50cd00915106e68c77e509ef030ae271ee2
jedlin21 <jedlikowski.pat@gmail.com>
2017-08-19 09:37:05 +0200

    Responsivenes properties changed; main_board first_table done

--- GitT/js/main.js
+++ GitT/js/main.js
@@ -3,9 +3,29 @@ $(document).ready(function(){
     var $middle_blockHeight = $(".middle_block").width * 1.2;
     $(".middle_block").css("height",$middle_blockHeight + "px");
 
-   $(window).on('resize',funtion(){
 
-    })
+ var isBig = 1;
+ var $window = $(window);
+ if($window.width()<992){
+ $(".main_board").addClass("container-fluid");
+ isBig = 0;
+ }
+ else {
+ $(".main_board").addClass("container");
+ }
+
+ $window.on('resize',funtion() {
+ if ((this.width() < 992) && (isBig)) {
+ $(".main_board").removeClass("container");
+ $(".main_board").addClass("container-fluid");
+ }
+ else if((this.width() >= 992) && !(isBig)) {
+ $(".main_board").removeClass("container-fluid");
+ $(".main_board").addClass("container");
+ }
+ });
+
+
  */
     $(".middle_block").mouseenter(function() {
         $(".img_changer",this).css("background-color", "rgba(83, 167, 70, 0.6)");
@@ -15,6 +35,5 @@ $(document).ready(function(){
         $(".img_changer",this).css("background-color", "rgba(0, 0, 0, 0.6)");
         $("h3", this).css("text-decoration", "none");
     });
-
 
 });
