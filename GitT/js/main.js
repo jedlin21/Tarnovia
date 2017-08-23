665018352e00b8bf66d8819082081f08039d848f
jedlin21 <jedlikowski.pat@gmail.com>
2017-08-23 15:56:56 +0200

    Left_part main_board Facebook ikon added; middle blocks main_board hover improved; main_board done

--- GitT/js/main.js
+++ GitT/js/main.js
@@ -2,37 +2,15 @@ $(document).ready(function(){
 /*
     var $middle_blockHeight = $(".middle_block").width * 1.2;
     $(".middle_block").css("height",$middle_blockHeight + "px");
-
-
- var isBig = 1;
- var $window = $(window);
- if($window.width()<992){
- $(".main_board").addClass("container-fluid");
- isBig = 0;
- }
- else {
- $(".main_board").addClass("container");
- }
-
- $window.on('resize',funtion() {
- if ((this.width() < 992) && (isBig)) {
- $(".main_board").removeClass("container");
- $(".main_board").addClass("container-fluid");
- }
- else if((this.width() >= 992) && !(isBig)) {
- $(".main_board").removeClass("container-fluid");
- $(".main_board").addClass("container");
- }
- });
-
-
  */
+
+
     $(".middle_block").mouseenter(function() {
-        $(".img_changer",this).css("background-color", "rgba(83, 167, 70, 0.6)");
+        $(".img_changer",this).animate({opacity: '0.5'}, "fast");
         $("h3", this).css("text-decoration", "underline");
     });
     $(".middle_block").mouseleave(function() {
-        $(".img_changer",this).css("background-color", "rgba(0, 0, 0, 0.6)");
+        $(".img_changer",this).animate({opacity: '0'}, "fast");
         $("h3", this).css("text-decoration", "none");
     });
 
